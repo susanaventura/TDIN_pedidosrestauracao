104b58f090c65168783dbcb067400ad5ebd60c2f
Susana <si.ventura1@gmail.com>
2016-03-28 10:04:26 +0100

    terminais base

---
+++ RestaurantService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantService
+{
+    static class Program
+    {
+        
+ 
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            RemotingConfiguration.Configure("RestaurantService.exe.config", false);
+            Console.WriteLine("[RestaurantService] hosting Register");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
